2f920d241406dd5995aec4609f0291e5b7515485
Modulus <thejohnskauge@gmail.com>
2018-10-16 22:34:39 +0200

    Timer in place

--- src/main.elm
+++ src/main.elm
@@ -5,6 +5,8 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 import Http exposing (get)
+import Time
+import Task
 
 main =
   Browser.element
@@ -14,16 +16,28 @@ main =
     , view = view
     }
 
-type alias Model = { version: String, meta: String}
+type alias Model = 
+        { 
+        version: String
+        , meta: String  
+        , zone: Time.Zone
+        , time: Time.Posix
+        }
     
 type Msg 
     = Update 
+    | Reset
+    | Tick Time.Posix
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         Update -> 
            ({ model | version = "jaadda" }, Cmd.none)
+        Tick newTime ->
+            ({model | time = newTime }, Cmd.none)
+        Reset -> 
+            ({ model | zone = Time.utc, time =  (Time.millisToPosix 0) }, Cmd.none)
 
 -- MODEL
 
@@ -32,15 +46,17 @@ url = "http://localhost:5000"
 
 init :  () -> (Model, Cmd Msg) 
 init _ =
-    (Model "0.0" "Nothing", Cmd.none)
+    (Model "Version: N/A" "Nothing" Time.utc (Time.millisToPosix 0),  Cmd.none)
 
   
 view : Model -> Html Msg
 view model = 
 
     div  [][
-             div [][ text model.version ] 
+            div [][ text (String.fromInt ( Time.toSecond model.zone model.time)) ] 
+            ,div [][ text model.version ] 
             ,button  [onClick Update ] [text "Update"]
+            ,button [onClick Reset] [text "Reset"]
     ]
 
 
@@ -50,5 +66,5 @@ view model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+  Time.every 1000 Tick
     
