d7211423688ab7b582d510d5bb9d567ff4dbd661
John Sigvald Skauge <thejohnskauge@gmail.com>
2018-10-16 19:38:49 +0200

    Broken, commiting to continue work on other machine

--- src/main.elm
+++ src/main.elm
@@ -4,12 +4,14 @@ import Time exposing (now, every)
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Http exposing (get)
 
 main =
-  Browser.sandbox
+  Browser.element
     { init = init
-    , view = view
     , update = update
+    , subscriptions = subscriptions
+    , view = view
     }
 
 type alias Model = { version: String, meta: String}
@@ -17,27 +19,28 @@ type alias Model = { version: String, meta: String}
 type Msg 
     = Update 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         Update -> 
-           { model | version = "jaadda" }
+           ({ model | version = "jaadda" }, Cmd.none)
 
 -- MODEL
 
+url : String
+url = "http://localhost:5000"
 
-init : Model 
+init :  (Model, Cmd Msg) 
 init =
-    Model "0.0" "Nothing"
+    (Model "0.0" "Nothing", Cmd.none)
 
   
 view : Model -> Html Msg
 view model = 
-    div []
-    [
-        div  [][
-            button  [onClick Update ] [text "Update"]
-        ]
+
+    div  [][
+             div [][ text model.version ] 
+            ,button  [onClick Update ] [text "Update"]
     ]
 
 
@@ -45,6 +48,7 @@ view model =
     --     , button [onClick Update] [ text "Get version" ]
     -- ]
 
---subscriptions : Model -> Msg
---subscriptions = 0
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
     
