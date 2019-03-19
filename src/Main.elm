953e77cf2a88c5493d4b9a85f6ea2a20f2f22e0d
John Sigvald Skauge <thejohnskauge@gmail.com>
2019-03-19 13:36:06 +0100

    Set backend to localhost

--- src/Main.elm
+++ src/Main.elm
@@ -22,7 +22,7 @@ main =
     -- MODEL
 
 url : String
-url = "http://version.snadder.io/api" --"http://localhost:5000"
+url = "http://localhost:5000"  -- ""http://version.snadder.io/api" --"http://localhost:5000"
 
 
 versionTypeDecoder : Decoder VersionJson 
